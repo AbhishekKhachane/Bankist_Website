4409e6e783c56ce4c016bb95c9f6f30e941f390f
AbhishekKhachane <khachaneabhishek23@gmail.com>
2023-02-28 10:07:30 +0530

    Selecting,Creating and Inserting Elements

--- script.js
+++ script.js
@@ -33,3 +33,44 @@ document.addEventListener("keydown", function (e) {
     closeModal();
   }
 });
+
+///////////////////////////////////////////////
+// Selecting Elements
+console.log(document.documentElement);
+console.log(document.head);
+console.log(document.body);
+
+const header = document.querySelector(".header");
+const allSections = document.querySelectorAll(".section");
+console.log(allSections);
+
+document.getElementById("section--1");
+const allButtons = document.getElementsByTagName("button");
+console.log(allButtons);
+
+console.log(document.getElementsByClassName("btn"));
+
+///////////////////////////////////////////////
+// Creating and Inserting Elements
+const message = document.createElement("div");
+message.classList.add("cookie-message");
+// message.textContent = `We use cookies for improved functionality and analytics.`;
+message.innerHTML = `We use cookies for improved functionality and analytics. <button class="btn btn--close-cookie">Got it !!</button>`;
+
+// header.prepend(message);
+header.append(message); // Here we are not able to see the message at both places in header. We can just see the appended one and not the prepended one.
+// This is because it moves the element and not copy it.To do so we need to first clone as shown :-
+
+// header.append(message.cloneNode(true)); // Now we can see the msg at both places
+
+// Also we have before and after
+// header.before(message);
+// header.after(message);
+
+///////////////////////////////////////////////
+// Deleting Elements
+document
+  .querySelector(".btn--close-cookie")
+  .addEventListener("click", function () {
+    message.remove();
+  });

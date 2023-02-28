73a5eb727e4e9def2b3c43cdecb0d998ac45ad0f
AbhishekKhachane <khachaneabhishek23@gmail.com>
2023-02-28 13:50:24 +0530

    Event Delegation

--- script.js
+++ script.js
@@ -8,6 +8,9 @@ const overlay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector(".btn--close-modal");
 const btnsOpenModal = document.querySelectorAll(".btn--show-modal");
 
+const btnScrollTo = document.querySelector(".btn--scroll-to");
+const section1 = document.querySelector("#section--1");
+
 const openModal = function (e) {
   e.preventDefault();
   modal.classList.remove("hidden");
@@ -31,6 +34,35 @@ overlay.addEventListener("click", closeModal);
 document.addEventListener("keydown", function (e) {
   if (e.key === "Escape" && !modal.classList.contains("hidden")) {
     closeModal();
+  }
+});
+
+///////////////////////////////////////////////
+// Smooth Scrolling ---> Learn More(Button)
+btnScrollTo.addEventListener("click", function (e) {
+  section1.scrollIntoView({ behavior: "smooth" });
+});
+
+///////////////////////////////////////////////
+// Page Navigation
+// document.querySelectorAll(".nav__link").forEach(function (el) {
+//   el.addEventListener("click", function (e) {
+//     e.preventDefault();
+//     const id = this.getAttribute("href");
+//     document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+//   });
+// });
+
+// Event Delegation
+// 1. Add Event Listener to common parent
+// 2. Determine what element originated the event
+document.querySelector(".nav__links").addEventListener("click", function (e) {
+  // console.log(e.target);
+  e.preventDefault();
+  // Matching Strategy
+  if (e.target.classList.contains("nav__link")) {
+    const id = e.target.getAttribute("href");
+    document.querySelector(id).scrollIntoView({ behavior: "smooth" });
   }
 });
 
@@ -113,55 +145,58 @@ document.addEventListener("keydown", function (e) {
 // logo.classList.contains();
 
 ///////////////////////////////////////////////
-// Smooth Scrolling
-const btnScrollTo = document.querySelector(".btn--scroll-to");
-const section1 = document.querySelector("#section--1");
+// // Smooth Scrolling
+// const btnScrollTo = document.querySelector(".btn--scroll-to");
+// const section1 = document.querySelector("#section--1");
 
-btnScrollTo.addEventListener("click", function (e) {
-  // const s1coords = section1.getBoundingClientRect();
-  // console.log(s1coords);
+// btnScrollTo.addEventListener("click", function (e) {
+// const s1coords = section1.getBoundingClientRect();
+// console.log(s1coords);
 
-  // console.log(e.target.getBoundingClientRect());
+// console.log(e.target.getBoundingClientRect());
 
-  // console.log("Current Scroll (X/Y) ", window.pageXOffset, window.pageYOffset);
+// console.log("Current Scroll (X/Y) ", window.pageXOffset, window.pageYOffset);
 
-  // console.log(
-  //   "height/width viewport",
-  //   document.documentElement.clientHeight,
-  //   document.documentElement.clientWidth
-  // );
+// console.log(
+//   "height/width viewport",
+//   document.documentElement.clientHeight,
+//   document.documentElement.clientWidth
+// );
 
-  // Scroll
-  // window.scrollTo(
-  //   s1coords.left + window.pageXOffset,
-  //   s1coords.top + window.pageYOffset
-  // );
+// Scroll
+// window.scrollTo(
+//   s1coords.left + window.pageXOffset,
+//   s1coords.top + window.pageYOffset
+// );
 
-  // // Smooth
-  // window.scrollTo({
-  //   left: s1coords.left + window.pageXOffset,
-  //   top: s1coords.top + window.pageYOffset,
-  //   behavior: "smooth",
-  // });
+// // Smooth
+// window.scrollTo({
+//   left: s1coords.left + window.pageXOffset,
+//   top: s1coords.top + window.pageYOffset,
+//   behavior: "smooth",
+// });
 
-  section1.scrollIntoView({ behavior: "smooth" });
-});
+//   section1.scrollIntoView({ behavior: "smooth" });
+// });
 
 ///////////////////////////////////////////////
 // Events types and Event Handlers
-const h1 = document.querySelector("h1");
+// const h1 = document.querySelector("h1");
 
-const alertH1 = function (e) {
-  alert("AddEventListener ");
-};
+// const alertH1 = function (e) {
+//   alert("AddEventListener ");
+// };
 
-h1.addEventListener("mouseenter", alertH1);
+// h1.addEventListener("mouseenter", alertH1);
 
-setTimeout(() => {
-  h1.removeEventListener("mouseenter", alertH1);
-}, 3000);
+// setTimeout(() => {
+//   h1.removeEventListener("mouseenter", alertH1);
+// }, 3000);
 
 // Old Way
 // h1.onmouseenter = function (e) {
 //   alert("OnMouseEnter ");
 // };
+
+///////////////////////////////////////////////
+//

45e99874fb245c99049314ec3a27f7daa455ec50
AbhishekKhachane <khachaneabhishek23@gmail.com>
2023-02-28 15:49:59 +0530

    Building Tabbed Component

--- script.js
+++ script.js
@@ -67,6 +67,47 @@ document.querySelector(".nav__links").addEventListener("click", function (e) {
 });
 
 ///////////////////////////////////////////////
+// Tabbed Component
+const tabs = document.querySelectorAll(".operations__tab");
+const tabsContainer = document.querySelector(".operations__tab-container");
+const tabsContent = document.querySelectorAll(".operations__content");
+
+// BAD PRACTICE
+// tabs.forEach((t) => {
+//   t.addEventListener("click", function () {
+//     console.log("object");
+//   });
+// });
+
+// GOOD PRACTICE  --> Add addEventListener to common parent
+tabsContainer.addEventListener("click", function (e) {
+  // Matching Strategy
+  const clicked = e.target.closest(".operations__tab");
+
+  // Guard Clause
+  if (!clicked) {
+    return;
+  }
+
+  // Remove active classes
+  tabs.forEach((t) => {
+    t.classList.remove("operations__tab--active");
+  });
+
+  tabsContent.forEach((c) => {
+    c.classList.remove("operations__content--active");
+  });
+
+  // Activate Tab
+  clicked.classList.add("operations__tab--active");
+
+  // Activate Content area
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add("operations__content--active");
+});
+
+///////////////////////////////////////////////
 // Selecting Elements
 // console.log(document.documentElement);
 // console.log(document.head);
